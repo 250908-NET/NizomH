6ec496d222411ec1ef4cb856e7f845cededc4e5f
Husan Nizom <husan.dev@h-n.tech>
2025-09-16 18:53:38 -0400

    Completed classes coding challenges

--- Week1/CodingChallenges/9_Classes/9_Classes/Human.cs
+++ Week1/CodingChallenges/9_Classes/9_Classes/Human.cs
@@ -6,5 +6,20 @@ namespace _9_ClassesChallenge
 {
     internal class Human
     {
+        protected string _firstName = "Pat";
+        protected string _lastName = "Smyth";
+        public Human(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+        public Human()
+        {
+        }
+
+        public string AboutMe()
+        {
+            return $"My name is {_firstName} {_lastName}.";
+        }
     }//end of class
 }//end of namespace
--- Week1/CodingChallenges/9_Classes/9_Classes/Human2.cs
+++ Week1/CodingChallenges/9_Classes/9_Classes/Human2.cs
@@ -4,8 +4,64 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("9_ClassesChallenge.Tests")]
 namespace _9_ClassesChallenge
 {
-    internal class Human2
+    internal class Human2: Human
     {
+        private string _eyeColor;
+        private int _age;
+        private int _weight;
+
+        public Human2(string firstName, string lastName, string eyeColor, int age)
+        {
+
+            _firstName = firstName;
+            _lastName = lastName;
+            _eyeColor = eyeColor;
+            _age = age;
+        }
+        public Human2(string firstName, string lastName, string eyeColor)
+        {
+
+            _firstName = firstName;
+            _lastName = lastName;
+            _eyeColor = eyeColor;
+        }
+        public Human2(string firstName, string lastName, int age)
+        {
+
+            _firstName = firstName;
+            _lastName = lastName;
+            _age = age;
+        }
+        public Human2() {}
+
+        public string AboutMe2()
+        {
+            string info = $"My name is {_firstName} {_lastName}.";
+            if (_age != 0)
+            {
+                info += $" My age is {_age}.";
+            }
+            if (_eyeColor != null)
+            {
+                info += $" My eye color is {_eyeColor}.";
+            }
+            return info;
+        }
+        public int Weight
+        {
+            get { return _weight; }
+            set
+            {
+                if (value < 0 || value > 400)
+                {
+                    _weight = 0;
+                }
+                else
+                {
+                    _weight = value;
+                }
+            }
+        }
 
     }
 }
